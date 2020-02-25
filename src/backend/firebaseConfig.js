e0fabd8cd25cd824dffd70d5749ba967dec6b168
GoinPay <Goin.Payment@gmail.com>
2020-02-24 21:30:13 -0500

    Switch firebase config to other config

--- src/backend/firebaseConfig.js
+++ src/backend/firebaseConfig.js
@@ -1,10 +1,21 @@
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyB3QKn3wWwCmbd7NccjF-DT3YhtXSHAKCE",
+//   authDomain: "split-f7881.firebaseapp.com",
+//   databaseURL: "https://split-f7881.firebaseio.com",
+//   projectId: "split-f7881",
+//   storageBucket: "split-f7881.appspot.com",
+//   messagingSenderId: "725396944439",
+//   appId: "1:725396944439:web:2ac3df711c9b8379c131b2",
+//   measurementId: "G-H4SR5FXSL0"
+// };
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyB3QKn3wWwCmbd7NccjF-DT3YhtXSHAKCE",
-  authDomain: "split-f7881.firebaseapp.com",
-  databaseURL: "https://split-f7881.firebaseio.com",
-  projectId: "split-f7881",
-  storageBucket: "split-f7881.appspot.com",
-  messagingSenderId: "725396944439",
-  appId: "1:725396944439:web:2ac3df711c9b8379c131b2",
-  measurementId: "G-H4SR5FXSL0"
+  apiKey: "AIzaSyCS7kcviO5nQg3fNHOXg-R6_vQFhsmK3Tk",
+  authDomain: "goin-974a9.firebaseapp.com",
+  databaseURL: "https://goin-974a9.firebaseio.com",
+  projectId: "goin-974a9",
+  storageBucket: "goin-974a9.appspot.com",
+  messagingSenderId: "1043633240748",
+  appId: "1:1043633240748:web:5fcb79842dd39f7cf77d09",
+  measurementId: "G-9QKBNFGL5X"
 };
